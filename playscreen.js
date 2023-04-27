c0efcdd37250fddcd0cb0e2cf57542223e7352ac
Yoana <ivyo22hk@student.ju.se>
2023-04-27 13:36:00 +0200

    player 1

---
+++ playscreen.js
@@ -0,0 +1,101 @@
+const canvas = document.querySelector("canvas");
+
+//c = context
+const c = canvas.getContext("2d");
+
+canvas.width = 1024;
+canvas.height = 576;
+
+//player object
+const gravity = 0.5;
+
+class Player {
+  constructor(position) {
+    this.position = position;
+    this.velocity = {
+      x: 0,
+      y: 1,
+    };
+    this.height = 100;
+  }
+
+  draw() {
+    c.fillStyle = "red";
+    c.fillRect(this.position.x, this.position.y, 100, this.height);
+  }
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    if (this.position.y + this.height + this.velocity.y < canvas.height) {
+      this.velocity.y += gravity;
+    } else {
+      this.velocity.y = 0;
+    }
+  }
+}
+
+const player = new Player({
+  x: 0,
+  y: 0,
+});
+const player2 = new Player({
+  x: 924,
+  y: 0,
+});
+
+const keys = {
+  d: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+};
+
+//creating an always repeating animation
+function animate() {
+  window.requestAnimationFrame(animate);
+
+  c.fillStyle = "white";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+
+  player.update();
+  player2.update();
+
+  player.velocity.x = 0;
+  if (keys.d.pressed === true) {
+    player.velocity.x = 4;
+  } else if (keys.a.pressed === true) {
+    player.velocity.x = -4;
+  }
+}
+
+animate();
+
+//player1 keys
+window.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = true;
+      break;
+    case "a":
+      keys.a.pressed = true;
+      break;
+    case "w":
+      player.velocity.y = -15;
+      break;
+  }
+});
+
+window.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = false;
+      break;
+    case "a":
+      keys.a.pressed = false;
+      break;
+  }
+});
